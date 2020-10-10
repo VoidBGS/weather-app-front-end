bf68d840945827008037851ba1f6c4e2da26a758
VoidBGS <kristian_robert@abv.bg>
2020-10-10 21:30:39 +0300

    Removed unnecessary code

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PhotosTitle from './PhotosTitle'
 import './Photos.css'
-import { Row } from 'react-bootstrap';
 import ImageSection from './ImageSection'
 
 const Photos = () =>{
