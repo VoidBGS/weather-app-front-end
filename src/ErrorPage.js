3ebe4c66074dc1bf2c5eba23dae0dd7adf66efca
VoidBGS <kristian_robert@abv.bg>
2020-10-20 00:47:02 +0300

    Added Error Page for incorrect routes

---
+++ src/ErrorPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Row, Col, Button} from 'react-bootstrap'
+
+const ErrorPage  = () =>{
+    return(
+<Row className="pl-5">
+    <Col md={{ span: 2}}>
+    <p className="error-page-text">Error</p>
+    <p className="error-page-number pl-3">404</p>
+    </Col>
+    <Col>
+        <div className="error-page-message p-5">
+        <p><strong>Oops!</strong><br></br>
+         We could not find the page you were looking for :(</p>
+        </div>
+    </Col>
+</Row>
+    )
+}
+
+export default ErrorPage;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Forum from './components/Forum/Forum'
 import Photos from './components/Photos/Photos'
 import NewsArticleForm from './components/News/Form/NewsArticleForm'
 import Navigation from './components/Navigation'
+import ErrorPage from './ErrorPage'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 const CreateRoutes = () =>{
@@ -17,6 +18,7 @@ const CreateRoutes = () =>{
     <Route path="/News/Post" exact component={NewsArticleForm}/>
     <Route path="/Photos" exact component={Photos}/>
     <Route path="/Forum" exact component={Forum}/>
+    <Route component={ErrorPage} />
     </Switch>
     </Router>
     )
