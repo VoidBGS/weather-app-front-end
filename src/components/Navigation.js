cf6730c5822fa66755eac36c5f66f5e2ac7821b8
VoidBGS <kristian_robert@abv.bg>
2020-11-10 22:13:41 +0200

    Added Error Page and Pictures of the Day Responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,7 @@ const Navigation = () =>{
      <div>
          <Row className="navigation-flexbox">
              <Col sm={2} md={4} lg={6}><h2 className="logo">Weather<br/>Hoven</h2></Col>
-             <Col sm={{ span: 3, offset: 6 }} md={{ span: 3, offset: 4 }} lg={{ span: 2, offset: 4 }} ><p className="sign-in p-3"><a href="/Register">Sign up</a></p></Col>
+             <Col sm={{ span: 4, offset: 6 }} md={{ span: 4, offset: 4 }} lg={{ span: 2, offset: 4 }} ><p className="sign-in p-3"><a href="/Register">Sign up</a></p></Col>
          </Row>
          <Navbar expand="md">
          <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- src/components/Photos/ImageBox.js
+++ src/components/Photos/ImageBox.js
@@ -3,8 +3,8 @@ import { Col } from 'react-bootstrap';
 
 const ImageBox = () =>{
     return(
-         <Col xs={4}>
-         <div class="image-box">
+         <Col lg={{span:4}} >
+         <div className="image-box">
          <img src="https://i.ibb.co/p4HhZgQ/Mountain-and-waterfall-at-Logan-Pass-at-sunset-in-Glacier-National-Park-Montana.jpg"  alt="Problem with loading" style={{ width: '365px' }}></img>
          <p className="p-3">Posted by</p>
          </div>
