a9934b5b7452f60e0fc8b7e570d9eebc58dfd7f0
VoidBGS <kristian_robert@abv.bg>
2020-11-16 23:31:19 +0200

    Added Maps Page Design, Added 2 initial weather maps

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import MapsTitle from './MapsTitle.js'
+import './Maps.css'
 
 const Maps = () =>{
     return (
-        <Row className="justify-content-center">
-            <img src="https://images2.minutemediacdn.com/image/upload/c_fit,f_auto,fl_lossy,q_auto,w_728/v1555999902/shape/mentalfloss/construction_8.gif" alt="under-constuction" className="p-5"></img>
+        <>
+          <Row className="justify-content-center">
+           <MapsTitle></MapsTitle>
         </Row>
+        <Row className="map-page-wrapper m-3">
+        <p className="maps-secondary-title ml-4">Infrared Satellite Map - Europe</p>
+           <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=infrared" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
+           <hr className="linebreak-darkened"></hr>
+           <p className="maps-secondary-title ml-4">Satellite Map - Europe</p>
+           <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=visible" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
+           <hr className="linebreak-darkened"></hr>
+           <p className="maps-secondary-title ml-4">Satellite Map - Europe</p>
+           <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=visible" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
+           <hr className="linebreak-darkened"></hr>
+        </Row>
+        
+        </>
     )
 }
 
---
+++ src/components/Maps/MapsTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const MapsTitle = () =>{
+    {
+        return (
+            <p className="maps-title">Weather Maps and Images</p>
+        )
+    }
+}
+
+export default MapsTitle;
