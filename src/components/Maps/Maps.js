233365e5551db574099fefa870296581c6df018b
VoidBGS <kristian_robert@abv.bg>
2020-11-25 01:09:20 +0200

    Removed uneeded satellite map duplicate

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -10,11 +10,8 @@ const Maps = () =>{
            <MapsTitle></MapsTitle>
         </Row>
         <Row className="map-page-wrapper m-3">
-        <p className="maps-secondary-title ml-4">Infrared Satellite Map - Europe</p>
+           <p className="maps-secondary-title ml-4">Infrared Satellite Map - Europe</p>
            <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=infrared" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
-           <hr className="linebreak-darkened"></hr>
-           <p className="maps-secondary-title ml-4">Satellite Map - Europe</p>
-           <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=visible" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
            <hr className="linebreak-darkened"></hr>
            <p className="maps-secondary-title ml-4">Satellite Map - Europe</p>
            <Col><div className="infra-sat-map-wrapper m-4"><img src="https://api.met.no/weatherapi/geosatellite/1.4/?area=europe&type=visible" alt="meta-map" className="infra-sat-map p-4"></img></div></Col>
